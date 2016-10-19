a723a08db559cf92228aae300de84cd047de6d89
Dheinny Marques <dheinny@gmail.com>
2016-10-19 18:36:31 -0200

    Update PascalTriangle.java

--- PascalTriangle.java
+++ PascalTriangle.java
@@ -1,6 +1,5 @@
 public class PascalTriangle {
 	public static int factorial(int n, int l) {
-		//System.out.println(n +"-"+l);
 		if(n<=l) return 1;
 
 		return n*factorial(n-1,l);
@@ -10,9 +9,9 @@ public class PascalTriangle {
 		//base case:
 		if(r == 0) {System.out.print("1"); return;}
 
-        printLinePT(n, r-1);
-        //Recursive case:
-		//vai chamar n vezes a propria função e imprimir o valor da formula n! / (r! * (n-r)!)
+       		printLinePT(n, r-1);
+        	//Recursive case:
+		//vai chamar n vezes a propria funÃ§Ã£o e imprimir o valor da formula n! / (r! * (n-r)!)
 		int nrFact = factorial(n-r,1);
 		int rFact = nrFact*factorial(r, n-r);
     		int nFact = rFact * factorial(n, r); 
@@ -21,19 +20,18 @@ public class PascalTriangle {
 	}
 	
 	public static void printPT(int k) {
-		//receber o e fazer uma chamada pra cada linha, passando nk linha, e 1
+		//receber o k e fazer uma chamada pra cada linha, passando nk linha, e 1
 
 		//Base Case
 		if (k == 0) return;
 		printPT(k-1);
-        printLinePT(k-1, k-1); 
-        System.out.println("");
+        	printLinePT(k-1, k-1); 
+        	System.out.println("");
 
 		return;
 	}
 	
 	public static void main (String [] args) {
-		printPT(Integer.parseInt(args[0]));
-	
+		printPT(Integer.parseInt(args[0]));	
 	}
 }
