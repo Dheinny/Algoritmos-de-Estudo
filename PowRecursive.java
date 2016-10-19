5dd68e1755cfb8e129be763d88de93cd02fde064
Dheinny Marques <dheinny@gmail.com>
2016-10-19 18:38:59 -0200

    Update PowRecursive.java

--- PowRecursive.java
+++ PowRecursive.java
@@ -1,19 +1,15 @@
 public class PowRecursive{
 
-
 	public long calcPow(long base, long exp) {
 		if(exp == 0) {
 			return 1;
 		}
 		
 		return base * calcPow(base, exp-1);
-		
 	}
 
 	public static void main(String[] args) {
 		PowRecursive op = new PowRecursive();
 		System.out.println(op.calcPow(Long.parseLong(args[0]), Long.parseLong(args[1])));
-		
 	}	
-
 }
